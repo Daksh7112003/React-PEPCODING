2e0546c5612a5f28bde2ad4cd6e149bb97bab187
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2023-08-14 10:00:51 +0530

    Add files via upload

---
+++ map.js
@@ -0,0 +1,22 @@
+let arr= [1,2,3,4,5]
+let narr=[]
+//by normal method...
+for(let i=0; i<arr.length;i++){
+    narr[i]=arr[i]*2;
+
+}
+
+
+//bymap method...
+
+narr=arr.map((value,idx)=>{
+console.log(value,idx) //to print the value and the index of it 
+
+return value*2;
+})
+
+
+
+
+console.log(arr)
+console.log(narr)
